0f5cab6759b7072547f63e38aa5022f6d89e791e
Marie Crumpton <marie.k.cr@gmail.com>
2020-10-19 14:13:16 -0400

    fix: edit bone placement and receptiveness to user input

--- js/FoodTime.js
+++ js/FoodTime.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import { ViroARScene, ViroNode, Viro3DObject } from 'react-viro';
+import {
+  ViroARScene,
+  ViroNode,
+  Viro3DObject,
+  ViroImage,
+  ViroARCamera,
+  ViroText,
+} from 'react-viro';
 var createReactClass = require('create-react-class');
 
 export default FoodTime = createReactClass({
-	getInitialState() {
-		return {
-			dog: '',
-		};
-	},
+  getInitialState() {
+    return {
+      //dog: this.props.dog,
+      dog: '',
+    };
+  },
 
-	render() {
-		return (
-			<ViroARScene>
-				<ViroNode position={[0, -10, -20]} scale={[0.1, 0.1, 0.1]}>
-					<Viro3DObject
-						source={require('./res/dogColors/creamDog.vrx')}
-						position={[0, -10, -20]}
-						scale={[0.1, 0.1, 0.1]}
-						type="VRX"
-					/>
-					{/* <Viro3DObject
+  render() {
+    return (
+      <ViroARScene>
+        <ViroARCamera>
+          <ViroImage
+            height={0.5}
+            width={0.5}
+            source={require('./res/camera.png')}
+            position={[1.8, -3.4, -6]}
+            onDrag={this._onPress}
+          />
+          <ViroNode
+            dragType="FixedToWorld"
+            onDrag={() =>
+              this.props.arSceneNavigator.push({
+                scene: require('./BallThrowAR'),
+              })
+            }
+          >
+            <ViroImage
+              height={0.5}
+              width={0.5}
+              source={require('./res/bone.png')}
+              position={[-1.2, -2.8, -5]}
+            />
+          </ViroNode>
+        </ViroARCamera>
+        <ViroText
+          text={'On Food Time Page'}
+          scale={[1, 1, 1]}
+          position={[0, 0, -4]}
+        />
+        {/* <ViroNode position={[0, -10, -20]} scale={[0.1, 0.1, 0.1]}> */}
+
+        {/* <Viro3DObject
+            source={this.props.dog}
+            position={[0, -10, -20]}
+            scale={[0.1, 0.1, 0.1]}
+            type="VRX"
+          /> */}
+        {/* <Viro3DObject
 						source={require('./res/Dog-Graphics/DogBowl_NoFood.vrx')}
 						resources={[
 							require('./res/Dog-Graphics/Dogbowl2_UV.png'),
@@ -31,10 +69,14 @@ export default FoodTime = createReactClass({
 						scale={[0.02, 0.02, 0.02]}
 						type="VRX"
 					/> */}
-				</ViroNode>
-			</ViroARScene>
-		);
-	},
+        {/* </ViroNode> */}
+      </ViroARScene>
+    );
+  },
+  // _onPress() {
+  //   // alert('you pressed me');
+  //   this.props.arSceneNavigator.push({ scene: require('./BallThrowAR') });
+  // },
 });
 
 module.exports = FoodTime;
