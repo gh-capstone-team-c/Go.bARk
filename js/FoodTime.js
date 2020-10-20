80abba5ad42555e27cfe902f7fbd982e1b8bc712
Marie Crumpton <marie.k.cr@gmail.com>
2020-10-20 17:21:46 -0400

    chore: changing bone to bowl gif

--- js/FoodTime.js
+++ js/FoodTime.js
@@ -7,14 +7,21 @@ import {
   ViroARCamera,
   ViroText,
   ViroAmbientLight,
+  ViroAnimatedImage,
   ViroSpotLight,
 } from 'react-viro';
 var createReactClass = require('create-react-class');
 
-const dog = {
+const dogPose = {
   red: require('./res/dogPose/redDogEat.vrx'),
   blackTan: require('./res/dogPose/blackTanDogEat.vrx'),
   cream: require('./res/dogPose/creamDogEat.vrx'),
+};
+
+const dogStand = {
+  red: require('./res/dogColors/redDog.vrx'),
+  blackTan: require('./res/dogColors/blackTanDog.vrx'),
+  cream: require('./res/dogColors/creamDog.vrx'),
 };
 
 export default FoodTime = createReactClass({
@@ -47,15 +54,28 @@ export default FoodTime = createReactClass({
               })
             }
           >
-            <ViroImage
+            <ViroAnimatedImage
               height={0.05}
               width={0.05}
-              source={require('./res/bone.png')}
-              position={[-0.08, -0.16, -0.3]}
+              source={require('./res/dogBowlIcon.gif')}
+              // source={require('./res/bone.png')}
+              position={[-0.07, -0.16, -0.3]}
             />
           </ViroNode>
         </ViroARCamera>
         <ViroAmbientLight color={'#e8e0dc'} />
+
+        {/* food bowl & clicking on food bowl to get user points */}
+        <ViroNode
+          position={[0, -7, -10]}
+          scale={[0.19, 0.19, 0.19]}
+          onClick={() =>
+            this.state.addPoints({ points: this.state.user.points++ })
+          }
+        >
+          <Viro3DObject source={require('./res/dogBowl.vrx')} type="VRX" />
+        </ViroNode>
+
         {/* dog */}
         <ViroNode position={[0, -10, -20]} scale={[0.13, 0.13, 0.13]}>
           {/* NEED TO ADD SPOTLIGHT */}
@@ -75,19 +95,9 @@ export default FoodTime = createReactClass({
             source={dog[dogColor]}
             type="VRX"
           /> */}
-          <Viro3DObject source={dog[dogColor]} type="VRX" />
+          <Viro3DObject source={dogPose[dogColor]} type="VRX" />
         </ViroNode>
 
-        {/* food bowl & clicking on food bowl to get user points */}
-        <ViroNode
-          position={[0, -7, -10]}
-          scale={[0.19, 0.19, 0.19]}
-          onClick={() =>
-            this.state.addPoints({ points: this.state.user.points++ })
-          }
-        >
-          <Viro3DObject source={require('./res/dogBowl.vrx')} type="VRX" />
-        </ViroNode>
         <ViroText
           text={'Tap the bowl to feed your dog!'}
           scale={[1, 1, 1]}
