a81a0b654b93be9f0b8982aff938d683365af98d
Betsy Groton <grotoned@gmail.com>
2020-10-23 09:27:42 -0400

    feat: testing sockets in settings file

--- root-ios.js
+++ root-ios.js
@@ -4,8 +4,12 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import LoginIos from './js/Login-ios';
 import store from './store/store';
+import io from 'socket.io-client';
 
 export default class RootIos extends Component {
+	componentDidMount() {
+		io(store);
+	}
 	render() {
 		return (
 			<Provider store={store}>
